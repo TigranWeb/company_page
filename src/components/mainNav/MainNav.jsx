437df6ec566b1092e931d370653fd1eb6a6a7504
tigranWeb <tigrank.arm@gmail.com>
2022-12-25 21:53:52 +0100

    header styles

--- src/components/mainNav/MainNav.jsx
+++ src/components/mainNav/MainNav.jsx
@@ -1,6 +1,7 @@
 import "./mainNav.css";
 import { Link } from "react-router-dom";
-
+import { GiAbstract050 } from "react-icons/gi";
+import { FaChevronDown, FaUserAlt } from "react-icons/fa";
 
 const navData = [
   {
@@ -70,32 +71,32 @@ const navData = [
 
 export const MainNav = () => {
   return <nav className="mainNav_container">
-    <Link to="/" >
-      <img src="" alt="logo" />
-      <span> Logo Text</span>
+    <Link to="/" className="header_link header_logo" >
+      <GiAbstract050 color="#2BB371" />
+      <span> T/BLOG</span>
     </Link>
     <ul className="mainNav">
       {
         navData.map(data => {
           if (data.sub) {
             return (
-              <div key={data.id} className="mainNav_sub_container">
-                <span>{data.title}</span>
+              <li key={data.id} className="mainNav_sub_container">
+                <span className="header_link">{data.title} <FaChevronDown /></span>
                 <ul key={data.id} className="mainNav_sub">
                   {data.children.map(sub => {
                     return (
                       <li key={sub.id} className="mainNav_item">
-                        <Link to={sub.path}> {sub.name}</Link>
+                        <Link className="header_link_sub" to={sub.path}>  {sub.name} </Link>
                       </li>
                     )
                   })}
                 </ul>
-              </div>
+              </li>
             )
           } else {
             return (
               <li key={data.id} className="mainNav_item">
-                <Link to={data.path}> {data.name}</Link>
+                <Link className="header_link" to={data.path}> {data.name}</Link>
               </li>
             )
           }
@@ -106,9 +107,10 @@ export const MainNav = () => {
     {
       // has user
       false ?
-        <span>Log Out</span>
+        <button className="header_login header_link">Log Out</button>
         : (
-          <Link to="/login">
+          <Link className="header_login header_link" to="/login">
+            <FaUserAlt />
             <span> Sign In</span>
           </Link>
         )
