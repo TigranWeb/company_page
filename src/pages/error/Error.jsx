73e79c174a184cfec8187406595faf6abb10683d
tigranWeb <tigrank.arm@gmail.com>
2022-12-27 22:45:07 +0100

    page not found styles

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,16 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
+import "./error.css";
 
 export const Error = () => {
   const error = useRouteError();
-  console.error(error);
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="error-page">
+      <div className="error-content">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <Link to="/" className="error-link" >BECK TO HOME</Link>
+      </div>
     </div>
   );
 }
